351c392d242f07c9afba9f95c56635897212b117
Austin Kregel <austin@kbcomp.co>
2017-03-09 09:56:10 -0500

    Inital commit

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+require '../vendor/autoload.php';
+$dotenv = new Dotenv\Dotenv(__DIR__. '/../');
+$dotenv->load();
+$dotenv->required(['TITLE', '']);
+?><!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo getenv('TITLE'); ?></title>
+        <style>
+            body { background: #eeeeee; font-size: 23px; } .center-the-dang-thing { position:absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); font-family:monospace, sans-serif; text-align:center; }
+        </style>
+    </head>
+    <body>
+    <div class="center-the-dang-thing">
+        <h3>Greetings stranger!</h3>
+        <div>
+            We currently have this website on our todo list, so we'll be getting to this soon.
+        </div>
+    </div>
+    </body>
+</html>
